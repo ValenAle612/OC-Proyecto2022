35909b47a47b5db323198fd4341697d54a6699f0
Valentina Navarro <71287615+ValenAle612@users.noreply.github.com>
2022-10-20 13:09:15 -0300

    por ahi me mande cagadas

--- colacp.c
+++ colacp.c
@@ -1,31 +1,23 @@
-
-
-typedef void * TClave;
-typedef void * TValor;
-
-typedef struct entrada {
-    TClave clave;
-    TValor valor;
-} * TEntrada;
-
-typedef struct nodo {
-    TEntrada entrada;
-    struct nodo * padre;
-    struct nodo * hijo_izquierdo;
-    struct nodo * hijo_derecho;
-} * TNodo;
-
-typedef struct cola_con_prioridad {
-    int cantidad_elementos;
-    TNodo raiz;
-    int (*comparador)(TEntrada, TEntrada);
-} * TColaCP;
+#include "stdio.h"
+#include <stdlib.h>
+#include "colacp.h" //cola con prioridad header
 
 /**
     Crea y retorna una cola con prioridad vacía.
 */
 TColaCP crear_cola_cp(int (*f)(TEntrada, TEntrada)){
 
+   TColaCP * cp = malloc(sizeof(struct cola_con_prioridad));
+
+   if(*cp == NULL){
+        printf("ERROR: No se pudo reservar espacio en memoria.");
+   }
+
+   (*cp) -> cantidad_elementos = 0;
+   (*cp) -> raiz = malloc(sizeof(struct nodo));
+   (*cp) -> comparador = f;
+
+   return * cp;
 }
 
 /**
@@ -34,6 +26,46 @@ TColaCP crear_cola_cp(int (*f)(TEntrada, TEntrada)){
     -> Falso, caso contrario.
 */
 int cp_insertar(TColaCP cola, TEntrada entr){
+
+    int p, inserte = FALSE;
+    TNodo * n, * actual;
+
+    n = malloc(sizeof(struct nodo));
+    (*n) -> entrada = entr;
+
+    actual = cola -> raiz;
+
+    while(!inserte){
+
+        p = cola -> comparador( entr, (*actual) -> entrada); // prioridad p
+                        // si tiene hijoizq y no der o si tiene der y no izq hay que balancear?
+        switch( p ){//si tiene menor prioridad va en el hizq sino en el der hay que acomodar
+
+            case -1:// entrada entr tiene menor prioridad que la entrada actual
+                if( (*actual) -> hijo_izquierdo == NULL && (*actual) -> hijo_derecho != NULL ){
+                    (*n)-> padre = actual;
+                    (*actual) -> hijo_izquierdo = n;
+                    inserte = TRUE;
+                }else{
+                    if( (*actual) -> hijo_derecho == NULL ){//nunca se da el caso que actual tenga h_izq pero no h_der (arbol balancead)
+                       //hay que poner n como hijo del padre de actual (swipe n y actual)
+                       (*n) -> hijo_derecho = actual;//n es raiz del sub arbol con hijo der actual
+                        inserte = TRUE;
+                    }else{
+                        actual = (*actual) -> hijo_izquierdo;
+                    }
+                }
+                break;
+            case 0:// entradas actual y entr tienen la misma prioridad
+
+            case 1:// entrada entr tiene mayor prioridad que la entrada actual
+               if( (*actual) -> hijo_izquierdo == NULL && (*actual) -> hijo_derecho != NULL){
+
+               }
+
+        };
+    }
+
 
 }
 
--- colacp.h
+++ colacp.h
@@ -1,6 +1,31 @@
 #ifndef COLACP_H_INCLUDED
 #define COLACP_H_INCLUDED
 
+#define FALSE 0 //valor lógico falso
+#define TRUE 1 //valor lógico verdadero
+#define CCP_NO_INI 2 //Intento de acceso inválido sobre ColaCP sin inicializar
+#define POS_NULA NULL //Posición nula
+#define ELE_NULO NULL //Elemento nulo
 
+typedef void * TClave;
+typedef void * TValor;
+
+typedef struct entrada {
+    TClave clave;
+    TValor valor;
+} * TEntrada;
+
+typedef struct nodo {
+    TEntrada entrada;
+    struct nodo * padre;
+    struct nodo * hijo_izquierdo;
+    struct nodo * hijo_derecho;
+} * TNodo;
+
+typedef struct cola_con_prioridad {
+    int cantidad_elementos;
+    TNodo raiz;
+    int (*comparador)(TEntrada, TEntrada);
+} * TColaCP;
 
 #endif // COLACP_H_INCLUDED
